9747d0eef9573954b0e92ef06caf2ebce300204f
Samuel Giraldo <samgirari@gmail.com>
2025-05-07 21:27:24 -0500

    Index Categories

--- Program.cs
+++ Program.cs
@@ -14,7 +14,7 @@ namespace SupermarketWEB
 
             //Agregando el contexto SupermarketContext a la aplicación
             builder.Services.AddDbContext<SupermarketContext>(options =>
-                 options.UseSqlServer(builder.Configuration.GetConnectionString("SupermarketContext")));
+                 options.UseSqlServer(builder.Configuration.GetConnectionString("SupermarketDB")));
 
 
             var app = builder.Build();
