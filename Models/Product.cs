b6983d36d3cd6a1d101fbdc86c4d1aca8c37bb70
Samuel Giraldo <samgirari@gmail.com>
2025-05-09 19:48:02 -0500

    Index Products

--- Models/Product.cs
+++ Models/Product.cs
@@ -9,7 +9,8 @@ namespace SupermarketWEB.Models
         public int Id { get; set; } // Será la llave primaria
         public string Name { get; set; }
 
-        [Column(TypeName = "decimal(6,2")]
+        [Column(TypeName = "decimal(6,2)")]
+
         public decimal Price { get; set; }
         public int Stock { get; set; }
         public int CategoryId { get; set; } // Será la llave foránea
